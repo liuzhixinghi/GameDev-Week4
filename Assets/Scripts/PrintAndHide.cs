8b895599cb6eaed870d7424679a27654a79f4033
liuzhixinghi <591056733@qq.com>
2021-08-24 17:45:50 +0800

    100%

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,18 +6,30 @@ public class PrintAndHide : MonoBehaviour
 {
     private int i = 3;
     public Renderer rend;
+    private int ranNum;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ranNum = Random.Range(200,251);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gameObject.tag == "Red"&&i==100)
+        {
+            gameObject.SetActive(false);
+        }
+        if (gameObject.tag == "Blue" && i == ranNum)
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
+
+
         i++;
-        Debug.Log(gameObject.name + ":"+i);
+        Debug.Log(gameObject.name + ":" + i);
+
     }
 }
